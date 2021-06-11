3366d83006fd6771e5461258daa33294b87b31e4
Yelincode <yelincode@gmail.com>
2021-06-11 18:05:09 -0400

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ofcPokemon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "kimbab", "kimchi" };
+            return dishes;
+        }
+    }
+}
